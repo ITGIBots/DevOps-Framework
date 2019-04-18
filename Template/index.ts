a85de1a55d810b19bef1e85782d41042d2d8b046
gibots <gibots@gibotss-MacBook-Pro-2.local>
2019-04-18 15:35:47 +0530

    database connection

--- Template/index.ts
+++ Template/index.ts
@@ -20,8 +20,7 @@ bootstrapMicroframework([
     console.log('db Password '+ env.db.password);
     console.log('db Host '+ env.db.host);
     console.log('db Port'+ env.db.port);
-    //mongoose.connect(("mongodb://"+env.db.username+":"+env.db.password+"@"+env.db.host+":"+env.db.port+"/"+env.db.database), options);
-    mongoose.connect("mongodb://127.0.0.1:27017/cub",options);
+    mongoose.connect(("mongodb://"+env.db.username+":"+env.db.password+"@"+env.db.host+":"+env.db.port+"/"+env.db.database), options);
     mongoose.connection.on('error', (error) => {
         console.log(error);
         console.log('MongoDB connection error. Please make sure MongoDB is running.');
--- user_org/index.ts
+++ user_org/index.ts
@@ -15,13 +15,7 @@ bootstrapMicroframework([
     .then(() => console.log("Application is up and running."))
     .catch(error => console.log("Application is crashed: " + error));
 
-    console.log('connecting to db '+ env.db.database);
-    console.log('db Username '+ env.db.username);
-    console.log('db Password '+ env.db.password);
-    console.log('db Host '+ env.db.host);
-    console.log('db Port'+ env.db.port);
-    //mongoose.connect(("mongodb://"+env.db.username+":"+env.db.password+"@"+env.db.host+":"+env.db.port+"/"+env.db.database), options);
-    mongoose.connect("mongodb://127.0.0.1:27017/cub",options);
+    mongoose.connect(("mongodb://"+env.db.username+":"+env.db.password+"@"+env.db.host+":"+env.db.port+"/"+env.db.database), options);
     mongoose.connection.on('error', (error) => {
         console.log(error);
         console.log('MongoDB connection error. Please make sure MongoDB is running.');
