163ed128ff341a4eec096a9bd6ef5f8b1e649a0f
gibots <gibots@gibotss-MacBook-Pro-2.local>
2019-04-18 15:38:26 +0530

    modified

--- Template/index.ts
+++ Template/index.ts
@@ -15,11 +15,6 @@ bootstrapMicroframework([
     .then(() => console.log("Application is up and running."))
     .catch(error => console.log("Application is crashed: " + error));
 
-    console.log('connecting to db '+ env.db.database);
-    console.log('db Username '+ env.db.username);
-    console.log('db Password '+ env.db.password);
-    console.log('db Host '+ env.db.host);
-    console.log('db Port'+ env.db.port);
     mongoose.connect(("mongodb://"+env.db.username+":"+env.db.password+"@"+env.db.host+":"+env.db.port+"/"+env.db.database), options);
     mongoose.connection.on('error', (error) => {
         console.log(error);
